348e4ec62aa0d4d24a19dbff0e43a562018a6594
miio <info@miio.info>
2012-07-17 03:51:40 +0900

    インデントが残念な所が残っていたのでまたなおした

--- migrate/001_initialize_db.rb
+++ migrate/001_initialize_db.rb
@@ -1,17 +1,17 @@
 class InitializeDb < ActiveRecord::Migration
-	def up
-		create_table :kream_tracking_tables do |t|
-			t.string	:tag
-		end
-		create_table :kream_posts do |t|
-			t.integer	:twitter_id
-			t.string	:tweet
-			t.text		:tweet_obj
-			t.timestamps
-		end
-		create_table :kream_recorders do |t|
-			t.datetime	:start_date
-			t.datetime	:end_date
-		end
-	end
+  def up
+    create_table :kream_tracking_tables do |t|
+      t.string  :tag
+    end
+    create_table :kream_posts do |t|
+      t.integer  :twitter_id
+      t.string  :tweet
+      t.text    :tweet_obj
+      t.timestamps
+    end
+    create_table :kream_recorders do |t|
+      t.datetime  :start_date
+      t.datetime  :end_date
+    end
+  end
 end
