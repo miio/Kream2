7bba8ee0d0de37c0a6400793701a492cd1bad6cc
miio <info@miio.info>
2012-07-18 01:10:06 +0900

    ファイル付け忘れ

--- kream_backend.rb
+++ kream_backend.rb
@@ -2,9 +2,17 @@ require 'tweetstream'
 require 'pusher'
 require './kream_common.rb'
 
-TweetStream::Client.new.track('#testmiio', '#miiodev') do |status|
+TweetStream::Client.new.track('j-pop', '#miiodev') do |status|
   puts "#{status.id}/#{status.user.id}/#{status.text}"
-  pusher_status = {:status_id => status.id, :user_id => status.user.id, :text => status.text}
+  pusher_status = {
+    :status_id => status.id,
+    :user_id => status.user.id,
+    :user_name => status.user.name,
+    :screen_name => status.user.screen_name,
+    :text => status.text,
+    :profile_img => status.user.profile_image_url,
+    :client => status.source
+  }
   Pusher['broadcast'].trigger('an_event', pusher_status)
   KreamPost.transaction do
     t = KreamPost.new
